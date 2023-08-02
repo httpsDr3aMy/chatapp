6ed0b629bd454d9e37b1d7a262a71a5f945cb1f0
httpsDr3aMy <xkamilptx@gmail.com>
2023-08-02 16:30:20 +0200

    deploy next

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
-// vite.config.js
-
-export default {
+export default defineConfig({
+  plugins: [react()],
   // ...
-  build: {
-    rollupOptions: {
-      external: ['@fortawesome/fontawesome-svg-core'],
-    },
-  },
-};
+});
